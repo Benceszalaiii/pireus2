323e612e8af8be4761ab83c1ec3ee63118cc15c3
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2024-03-12 21:29:08 +0100

    PostCSS, TailwindCSS, cssnano support, kapcsolatok, etc.

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+function toggleDarkmode(){  // Handle toggle darkmode button
+    if (document.body.classList.contains("dark"))
+        document.body.classList.remove("dark")
+    else
+        document.body.classList.add("dark")
+}
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "selector",
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+}
